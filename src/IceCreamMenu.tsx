12eda57efeab2eea05ce9ec0d064850cc263aa86
dukartbr <dukartbrady@gmail.com>
2024-03-24 17:20:54 -0500

    wip

--- src/IceCreamMenu.tsx
+++ src/IceCreamMenu.tsx
@@ -3,6 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 export default function IceCreateMenu() {
   return (
     <Box>
+      {/* gh is insufferable */}
       <Text>Ice Cream Menu</Text>
     </Box>
   );
