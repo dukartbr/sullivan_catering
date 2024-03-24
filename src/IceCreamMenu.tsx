17c46554a0f9a6db0a3e24e95977378622c7a6d4
dukartbr <dukartbrady@gmail.com>
2024-03-24 17:22:22 -0500

    adds deploy yml

--- src/IceCreamMenu.tsx
+++ src/IceCreamMenu.tsx
@@ -3,7 +3,6 @@ import { Box, Text } from "@chakra-ui/react";
 export default function IceCreateMenu() {
   return (
     <Box>
-      {/* gh is insufferable */}
       <Text>Ice Cream Menu</Text>
     </Box>
   );
